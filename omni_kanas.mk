ab6d02d329902bd8fad272ad3e4c9023806990f7
MarvelMathesh <123v.mathesh@gmail.com>
2023-05-07 20:08:13 +0530

    Kanas: Update Kernel & Misc

--- omni_kanas.mk
+++ omni_kanas.mk
@@ -16,16 +16,14 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_small.mk)
 
 # Inherit from kanas device
 $(call inherit-product, device/samsung/kanas/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kanas
